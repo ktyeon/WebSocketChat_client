fb1c8190de8c5837ea85c51596038c7b8945d323
ktyeon <84512238+ktyeon@users.noreply.github.com>
2024-01-22 16:24:31 -0500

    fix the bug (database issue_When I enter the nickname, it said there is a duplicate index.)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
+import { useEffect, useState, setUser } from "react";
 import "./App.css";
 import socket from "./server";
 
 function App() {
+ const [user, setUser] = useState(null);
+  
+  useEffect(() => {
+
+    askUserName();
+
+  }, []);
+
+  const askUserName = () => {
+    const userName = prompt("Enter your nickname:");
+  
+    // Check if the user entered a valid username (not null and not an empty string)
+    if (userName !== null && userName.trim() !== "") {
+      console.log("Entered username:", userName);
+  
+      socket.emit("signin", userName, (res) => {
+        console.log("Server response:", res);
+  
+        if (res?.ok) {
+          setUser(res.data);
+        } else {
+          console.error("Error during signin:", res?.error);
+        }
+      });
+    } else {
+      console.log("Invalid username. Please enter a valid nickname.");
+      // Handle the case where the user did not enter a valid username
+      // You might want to show an error message to the user or take appropriate action
+    }
+  };
+
+ 
   return (
     <div>
       <div className="App"></div>
